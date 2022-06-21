7b01719306b0843c56d83f4a905a5cd75fcfd9aa
rhsagar145 <>
2022-06-21 18:06:10 -0400

    converted amount displayed

--- src/Currency.js
+++ src/Currency.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
 
 function Currency(props) {
 	const initialFormState = {
 		from: '',
 		to: '',
+		amount: '',
 	};
 
 	const [formState, setFormState] = useState(initialFormState);
 	const [data, setData] = useState(null);
+	const [converted, setConvert] = useState(null);
 
 	function handleSubmit(event) {
 		event.preventDefault();
@@ -17,28 +18,41 @@ function Currency(props) {
 		console.log('You clicked me');
 		console.log(formState.from);
 		console.log(formState.to);
-		setFormState(initialFormState);
+		console.log(formState.amount);
+
+		// console.log(converted);
+
+		// setFormState(initialFormState);
 	}
 
 	function getData() {
+		console.log('get data is running');
 		const url = `https://api.exchangerate.host/convert?from=${formState.from}&to=${formState.to}`;
+
 		fetch(url)
 			.then((res) => res.json())
 			.then((res) => {
-				setData(res);
+				// setData(res);
+				return setData(res);
+			})
+			.then((res) => {
+				const newData = data.info.rate * formState.amount;
+
+				setConvert(newData);
+				console.log(newData);
 			})
 			.catch((err) => console.log('Something went wrong', err));
 	}
 
 	useEffect(() => {
-		getData();
+		// getData();
 	}, []);
 
-	console.log(data);
+	// console.log(data);
 
-	if (!data) {
-		return <p>Loading information ...</p>;
-	}
+	// if (!data) {
+	// 	return <p>Loading information ...</p>;
+	// }
 
 	return (
 		<div>
@@ -53,7 +67,7 @@ function Currency(props) {
 						setFormState({ ...formState, from: event.target.value })
 					}
 				/>
-				<label htmlFor='to'>To:</label>
+				<label htmlFor='to'>To: </label>
 				<input
 					type='text'
 					id='to'
@@ -63,8 +77,18 @@ function Currency(props) {
 					}
 				/>
 				<button type='submit'>Confirm</button>
+				<label htmlFor='amount'>Amount: </label>
+				<input
+					type='text'
+					id='amount'
+					value={formState.amount}
+					onChange={(event) =>
+						setFormState({ ...formState, amount: event.target.value })
+					}
+				/>
 			</form>
-			<h1>The conversion rate is: {data.info.rate}</h1>
+			
+			<h1>The converted amount is: {data?.info.rate * formState.amount} </h1>
 		</div>
 	);
 }
